4f0fd4805053808c5d4f70b7f2afb2438184eef0
Gabija <gabija.savu@gmail.com>
2020-06-26 03:51:13 +0300

    fixed game bugs

--- amd/src/game.js
+++ amd/src/game.js
@@ -53,6 +53,11 @@ define(['Phaser', 'jquery', 'core/yui', 'core/notification', 'core/ajax'], funct
                 }
 
                 create() {
+                    this.score = 0;
+                    this.answersText = [];
+                    this.level = 1;
+                    this.corretMultipleAnswers = [];
+                    this.showNewQuestion = false;
                     this.add.image(400, 300, 'sky');
 
                     this.clouds = this.add.group();
@@ -129,9 +134,11 @@ define(['Phaser', 'jquery', 'core/yui', 'core/notification', 'core/ajax'], funct
                         this.player.setVelocityX(200);
                         this.player.anims.play('walk', true);
                     }
-                    else if (this.cursors.up.isDown && this.player.body.blocked.down) {
-                        this.player.setVelocityY(-600);
+                    else if (this.cursors.up.isDown) {
                         this.player.anims.play('jump', true);
+                        if (this.player.body.blocked.down) {
+                            this.player.setVelocityY(-600);
+                        }
                     }
                     else {
                         this.player.setVelocityX(0);
@@ -140,10 +147,11 @@ define(['Phaser', 'jquery', 'core/yui', 'core/notification', 'core/ajax'], funct
 
                     if (this.showNewQuestion) {
                         this.showNewQuestion = false;
+
+
                         this.time.addEvent({
                             delay: 1000,
                             callback: () => {
-
                                 this.clouds.clear(true, true);
                                 this.questionText.setText(this.questions[this.level - 1].question);
                                 for (var i in this.answersText) {
@@ -175,10 +183,11 @@ define(['Phaser', 'jquery', 'core/yui', 'core/notification', 'core/ajax'], funct
                                         text.setOrigin(0.5);
                                         this.answersText.push(text)
 
-                                        if (this.questions[this.level - 1].answers[i].fraction > 0) {
-                                            this.corretMultipleAnswers.push(i);
+                                        if (!this.questions[this.level - 1].single) {
+                                            if (this.questions[this.level - 1].answers[i].fraction > 0) {
+                                                this.corretMultipleAnswers.push(i);
+                                            }
                                         }
-
                                         position += gap;
                                     }
                                 }
@@ -187,40 +196,60 @@ define(['Phaser', 'jquery', 'core/yui', 'core/notification', 'core/ajax'], funct
                                 //     this.collider.destroy();
                                 // }
                             },
-                            loop: true
+                            loop: false
                         })
                     }
 
                     this.collider = this.physics.add.overlap(this.player, this.clouds, function (player, cloud) {
+                        var fraction = parseFloat(cloud.fraction);
                         this.answersText[cloud.index].destroy()
-                        this.score += parseFloat(cloud.fraction);
-                        cloud.destroy();
+                        this.score = (parseFloat(this.score) + fraction).toFixed(2);
+                        cloud.destroy(true, true);
                         this.scoreText.setText('Rezultatas: ' + this.score);
-                        if (this.corretMultipleAnswers.length > 1) {
-                            var answerIndex = this.corretMultipleAnswers.findIndex(answerIndex => answerIndex == cloud.index);
-                            this.corretMultipleAnswers = this.corretMultipleAnswers.splice(answerIndex, 1);
-                        }
-                        else {
-                            this.showNewQuestion = true;
-                        }
-                        if (this.questions[this.level - 1].type == "multichoice" && this.corretMultipleAnswers.length > 0 && cloud.fraction > 0) {
-                            if (this.corretMultipleAnswers.length == 1) {
-                                this.showNewQuestion = true;
-                            }
-                            else {
+                        if (fraction > 0) {
+                            if (this.questions[this.level - 1].type == "multichoice" && this.corretMultipleAnswers.length > 1) {
                                 var answerIndex = this.corretMultipleAnswers.findIndex(answerIndex => answerIndex == cloud.index);
                                 this.corretMultipleAnswers = this.corretMultipleAnswers.splice(answerIndex, 1);
                             }
-                        }
-                        else {
-                            if (this.level >= this.questions.length) {
-                                this.endGame();
-                            }
                             else {
-                                this.level++;
-                                this.showNewQuestion = true;
+                                if (this.level >= this.questions.length) {
+                                    this.endGame();
+                                }
+                                else {
+                                    this.level++;
+                                    this.showNewQuestion = true;
+                                }
                             }
                         }
+
+                        // if (this.corretMultipleAnswers.length > 1) {
+                        //     var answerIndex = this.corretMultipleAnswers.findIndex(answerIndex => answerIndex == cloud.index);
+                        //     this.corretMultipleAnswers = this.corretMultipleAnswers.splice(answerIndex, 1);
+                        // }
+                        // else {
+                        //     this.level++;
+                        //     this.showNewQuestion = true;
+                        // }
+                        // if (this.questions[this.level - 1].type == "multichoice" && this.corretMultipleAnswers.length > 0 && cloud.fraction > 0) {
+                        //     if (this.corretMultipleAnswers.length == 1) {
+                        //         this.level++;
+                        //         this.showNewQuestion = true;
+                        //     }
+                        //     else {
+                        //         var answerIndex = this.corretMultipleAnswers.findIndex(answerIndex => answerIndex == cloud.index);
+                        //         this.corretMultipleAnswers = this.corretMultipleAnswers.splice(answerIndex, 1);
+                        //     }
+                        // }
+                        // else {
+                        //     console.log("collider level " + this.level)
+                        //     if (this.level >= this.questions.length) {
+                        //         this.endGame();
+                        //     }
+                        //     else {
+                        //         this.level++;
+                        //         this.showNewQuestion = true;
+                        //     }
+                        // }
                     }, null, this)
                 }
 
@@ -258,16 +287,21 @@ define(['Phaser', 'jquery', 'core/yui', 'core/notification', 'core/ajax'], funct
                 // }
 
                 endGame() {
+                    var grade;
+                    if (this.score > 0) {
+                        grade = this.score * 10 / this.questions.length;
+                        grade = Math.round(grade);
+                    }
                     ajax.call([{
                         methodname: 'mod_edugame_add_score',
-                        args: { edugameid: this.edugameId, score: Math.trunc(this.score) },
+                        args: { edugameid: this.edugameId, score: Math.trunc(grade) },
                         fail: notification.exception
                     }]);
                     this.scene.start('ResultScreen',
                         {
                             questions: this.questions,
                             edugameId: this.edugameId,
-                            score: this.score
+                            score: grade
                         })
                 }
             }
